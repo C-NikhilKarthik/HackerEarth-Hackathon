e72bc08f43cc83ecdc399d91f6f0b52dee2fdca1
C-NikhilKarthik <nikhilkarthik241103@gmail.com>
2023-11-15 23:11:58 +0530

    feat: client added

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <div className="w-full p-2 flex fixed top-0 left-0 shadow-lg justify-center">
+      <img
+        className="h-[50]"
+        src={
+          "https://resources.pulse.icc-cricket.com/whitelabel-assets/100364/logo-light.svg"
+        }
+        alt="Main Logo"
+      />
+    </div>
+  );
+}
+
+export default Navbar;
