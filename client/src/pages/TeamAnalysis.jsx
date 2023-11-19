8224c3d76ea52f3f605429ad43f5eae4267b76c8
C-NikhilKarthik <nikhilkarthik241103@gmail.com>
2023-11-19 18:39:25 +0530

    feat: team analysis

--- client/src/pages/TeamAnalysis.jsx
+++ client/src/pages/TeamAnalysis.jsx
@@ -28,16 +28,27 @@ const teamFlags = {
   Netherlands: netherlands,
 };
 
-const TeamAnalysis= () => {
-  const [selectedTeam1, setSelectedTeam1] = useState([]);
-  const [selectedTeam2, setSelectedTeam2] = useState([]);
-  const [Data,SetData] = useState({
-    innings: 0,
-    no_of_balls: 0,
-  })
+const TeamAnalysis = () => {
   const [isRadioVisible, setIsRadioVisible] = useState({
     Team1: true,
-    Team2: true
+    Team2: true,
+  });
+
+  const [data, setData] = useState({
+    Team1: {
+      Innings: 0,
+      Name: "",
+      Players: [],
+      Bowlers: {},
+      Overs: 50,
+    },
+    Team2: {
+      Innings: 0,
+      Name: "",
+      Players: [],
+      Bowlers: {},
+      Overs: 50,
+    },
   });
 
   const handleIconClick = (team) => {
@@ -47,98 +58,70 @@ const TeamAnalysis= () => {
     }));
   };
 
-  function getTeam1Batting(){
-    try{
-      axios.post("http://localhost:5000/predict_bowler_economy",{
-        "match_id": 65646,
-        "venue": "Bellerive Oval",
-        "innings": Data.innings,
-        "batting_team": data.Team2.Name,
-        "bowler": data.Team1.Player
-      }).then((response) => {
-        console.log(response)
-      }).catch((error)=> {
-        console.log(error)
-      })
-    }
-    catch(err){
-      console.log(err)
-    }
-  }
+  const handleTeamChange = (teamName, teamNo) => {
+    const isUniqueName = Object.values(data).every((team) => team.Name !== teamName);
 
-  const [data, setData] = useState({
-    Team1: {
-      Name: "",
-    },
-    Team2: {
-      Name: "",
-    },
-  });
-
-  const handleTeam1Change = (selectedTeam) => {
-    if (selectedTeam !== data.Team2.Name) {
+    if (isUniqueName) {
       setData((prevData) => ({
         ...prevData,
-        Team1: {
-          ...prevData.Team2,
-          Name: selectedTeam,
+        [teamNo]: {
+          ...prevData[teamNo],
+          Name: teamName,
+          Players: Players.find((team) => team.team === teamName)?.players || [],
         },
       }));
     } else {
-      // Optionally, you can display an error message or take another action
-      window.alert("Team1 and Team2 cannot be the same!");
+      window.alert("Team names must be unique!");
     }
   };
 
-  const handleTeam2Change = (selectedTeam) => {
-    // Check if Team1 and Team2 are not the same
-    if (selectedTeam !== data.Team1.Name) {
-      setData((prevData) => ({
-        ...prevData,
-        Team2: {
-          ...prevData.Team2,
-          Name: selectedTeam,
-        },
-      }));
+  const handleCheckboxChange = (bowler, teamNo) => {
+    const selectedBowlers = data[teamNo].Bowlers || {};
+    const updatedBowlers = { ...selectedBowlers };
+
+    // Toggle the selection of the bowler
+    if (updatedBowlers[bowler]) {
+      delete updatedBowlers[bowler];
     } else {
-      // Optionally, you can display an error message or take another action
-      window.alert("Team1 and Team2 cannot be the same!");
+      updatedBowlers[bowler] = 0; // Initialize overs to 0
     }
-  };
 
-  const handleTeam1Player = (data) => {
     setData((prevData) => ({
       ...prevData,
-      Team1: {
-        ...prevData.Team1,
-        Player: data,
+      [teamNo]: {
+        ...prevData[teamNo],
+        Bowlers: updatedBowlers,
       },
     }));
   };
 
-  const handleTeam2Player = (data) => {
-    setData((prevData) => ({
-      ...prevData,
-      Team2: {
-        ...prevData.Team2,
-        Player: data,
-      },
-    }));
+  const handleOversChange = (e, teamNo, bowler) => {
+    const newOvers = parseInt(e.target.value, 10);
+    
+    // Ensure the total overs for a bowler doesn't exceed 10
+    if (!isNaN(newOvers) && newOvers >= 0 && newOvers <= 10) {
+      setData((prevData) => ({
+        ...prevData,
+        [teamNo]: {
+          ...prevData[teamNo],
+          Bowlers: {
+            ...prevData[teamNo].Bowlers,
+            [bowler]: newOvers,
+          },
+        },
+      }));
+    }
   };
 
-  useEffect(() => {
-    if (data.Team1.Name) {
-      setSelectedTeam1(
-        Players.find((team) => team.team === data.Team1.Name)?.players || []
-      );
-    }
+  const calculateTotalSelectedOvers = (teamNo) => {
+    const selectedBowlers = data[teamNo].Bowlers || {};
+    return Object.values(selectedBowlers).reduce((totalOvers, overs) => totalOvers + overs, 0);
+  };
 
-    if (data.Team2.Name) {
-      setSelectedTeam2(
-        Players.find((team) => team.team === data.Team2.Name)?.players || []
-      );
-    }
-  }, [data.Team1.Name, data.Team2.Name]);
+  const isOversValid = (teamNo) => {
+    return calculateTotalSelectedOvers(teamNo) === data[teamNo].Overs;
+  };
+
 
 
   return (
@@ -167,21 +150,76 @@ const TeamAnalysis= () => {
               </div>
             </div>
             {isRadioVisible["Team1"] && (
-              <div className="flex w-full flex-wrap gap-4 p-4">
-                {Players.map((team, index) => (
-                  <label
-                    key={index}
-                    className="flex whitespace-nowrap items-center text-gray-400 gap-2 cursor-pointer"
-                  >
+              <div className="flex flex-col gap-8">
+                <div className="flex w-full flex-wrap gap-4 p-4">
+                  {Players.map((team, index) => (
+                    <label
+                      key={index}
+                      className="flex whitespace-nowrap items-center text-gray-400 gap-2 cursor-pointer"
+                    >
+                      <input
+                        type="radio"
+                        value={team.team}
+                        checked={data.Team1.Name === team.team}
+                        onChange={() => handleTeamChange(team.team, "Team1")}
+                      />
+                      {team.team}
+                    </label>
+                  ))}
+                </div>
+
+                <div className="flex flex-col p-4 gap-6">
+                  <h2 className="text-lg text-slate-100">Bowlers Selection</h2>
+                  <div>
+                    {data.Team1.Players.map((bowler, index) => (
+                      <div key={index} className="flex gap-4 text-gray-400">
+                        <input
+                          type="checkbox"
+                          id={index}
+                          checked={data.Team1.Bowlers && bowler in data.Team1.Bowlers}
+                          onChange={() => handleCheckboxChange(bowler, "Team1")}
+
+                        />
+                        <label htmlFor={index}>{bowler}</label>
+                        <input
+                          type="number"
+                          max={10}  
+                          className={`${data.Team1.Bowlers && bowler in data.Team1.Bowlers ? "flex" : "hidden"} w-14 rounded bg-slate-200/40`}
+                          onChange={(e) =>
+                            setData((prevData) => ({
+                              ...prevData,
+                              Team1: {
+                                ...prevData.Team1,
+                                Bowlers: {
+                                  ...prevData.Team1.Bowlers,
+                                  [bowler]: parseInt(e.target.value, 10) || 0, // Ensure it's a valid number
+                                },
+                              },
+                            }))
+                          }
+                        />
+
+                      </div>
+                    ))}
+                  </div>
+                  <div>
+                    <label>Total Overs:</label>
                     <input
-                      type="radio"
-                      value={team.team}
-                      checked={data.Team1.Name === team.team}
-                      onChange={() => handleTeam1Change(team.team)}
+                      type="number"
+                      value={data.Team1.Overs}  // Update this line
+                      onChange={(e) => handleOversChange(e, "Team1")}  // Pass teamNo to the handler
                     />
-                    {team.team}
-                  </label>
-                ))}
+                  </div>
+                  <div>
+                    <p>Total Selected Overs: {calculateTotalSelectedOvers("Team1")}</p>  {/* Pass teamNo to the function */}
+                    {isOversValid("Team1") ? (
+                      <p>Selection is valid!</p>
+                    ) : (
+                      <p style={{ color: 'red' }}>Total selected overs must be equal to {data.Team1.Overs}</p>
+                    )}
+                  </div>
+
+                </div>
               </div>
             )}
           </div>
@@ -209,68 +247,87 @@ const TeamAnalysis= () => {
               </div>
             </div>
             {isRadioVisible["Team2"] && (
-              <div className="flex w-full flex-wrap gap-4 p-4">
-                {Players.map((team, index) => (
-                  <label
-                    key={index}
-                    className="flex whitespace-nowrap items-center text-gray-400 gap-2 cursor-pointer"
-                  >
+              <div className="flex flex-col gap-8">
+
+                <div className="flex w-full flex-wrap gap-4 p-4">
+                  {Players.map((team, index) => (
+                    <label
+                      key={index}
+                      className="flex whitespace-nowrap items-center text-gray-400 gap-2 cursor-pointer"
+                    >
+                      <input
+                        type="radio"
+                        value={team.team}
+                        checked={data.Team2.Name === team.team}
+                        onChange={() => handleTeamChange(team.team, "Team2")}
+                      />
+                      {team.team}
+                    </label>
+                  ))}
+                </div>
+
+                <div className="flex flex-col p-4 gap-6">
+                  <h2 className="text-lg text-slate-100">Bowlers Selection</h2>
+                  <div>
+                    {data.Team2.Players.map((bowler, index) => (
+                      <div key={index} className="flex gap-4 text-gray-400">
+                        <input
+                          type="checkbox"
+                          id={`team2-${index}`}  // Use a unique identifier for Team2
+                          checked={data.Team2.Bowlers && bowler in data.Team2.Bowlers}
+                          onChange={() => handleCheckboxChange(bowler, "Team2")}
+                        />
+                        <label htmlFor={`team2-${index}`}>{bowler}</label>
+                        <input
+                          type="number"
+                          max={10}  
+                          className={`${data.Team2.Bowlers && bowler in data.Team2.Bowlers ? "flex" : "hidden"} w-14 rounded bg-slate-200/40`}
+                          onChange={(e) =>
+                            setData((prevData) => ({
+                              ...prevData,
+                              Team2: {
+                                ...prevData.Team2,
+                                Bowlers: {
+                                  ...prevData.Team2.Bowlers,
+                                  [bowler]: parseInt(e.target.value, 10) || 0, // Ensure it's a valid number
+                                },
+                              },
+                            }))
+                          }
+                        />
+                      </div>
+                    ))}
+                  </div>
+
+                  <div>
+                    <label>Total Overs:</label>
                     <input
-                      type="radio"
-                      value={team.team}
-                      checked={data.Team2.Name === team.team}
-                      onChange={() => handleTeam2Change(team.team)}
+                      type="number"
+                      value={data.Team2.Overs}
+                      onChange={(e) => handleOversChange(e, "Team2")}
                     />
-                    {team.team}
-                  </label>
-                ))}
+                  </div>
+                  <div>
+                    <p>Total Selected Overs: {calculateTotalSelectedOvers("Team2")}</p>
+                    {isOversValid("Team2") ? (
+                      <p>Selection is valid!</p>
+                    ) : (
+                      <p style={{ color: 'red' }}>Total selected overs must be equal to {data.Team2.Overs}</p>
+                    )}
+                  </div>
+
+                </div>
               </div>
             )}
           </div>
-
         </div>
 
-        <div className="flex gap-4">
-          <label className="text-slate-400">Innings</label>
-
-          <label
-            className="flex whitespace-nowrap items-center text-gray-400 gap-2 cursor-pointer"
-          >
-            <input
-              type="radio"
-              value="1st Innings"
-              checked={1 === Data.innings}
-              onChange={() => SetData((prevData)=>({
-                ...prevData,
-                innings:1
-              }))}
-            />
-            1st Innings
-          </label>
-
-          <label
-            className="flex whitespace-nowrap items-center text-gray-400 gap-2 cursor-pointer"
-          >
-            <input
-              type="radio"
-              value="2nd Innings"
-              checked={2 === Data.innings}
-              onChange={() => SetData((prevData)=>({
-                ...prevData,
-                innings:2
-              }))}            
-            />
-            2nd Innings
-          </label>
-        </div>
-
-
-        <button
-          onClick={getBatters}
+        {/* <button
+          onClick={getTeam1Batting}
           className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-md px-5 py-2.5 flex items-center mt-4"
         >
           Start Predicting <VscArrowCircleRight className="ml-2 text-3xl" />
-        </button>  
+        </button>   */}
 
       </div>
     </div>
